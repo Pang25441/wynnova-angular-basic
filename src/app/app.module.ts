fe178c50091c173bbb81f4bc141e92d71fd903d7
Pontep Hiranrat <Pang25441@users.noreply.github.com>
2022-03-03 14:08:19 +0700

    Finish Assignment 1

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HelloWorldComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
